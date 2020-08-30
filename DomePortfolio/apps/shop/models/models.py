9ea10a70293a232b4ae1307c5b402c167209a075
ThaRising <kochbe.ber@gmail.com>
2020-08-30 10:53:25 +0200

    Removed orders model

--- DomePortfolio/apps/shop/models/models.py
+++ DomePortfolio/apps/shop/models/models.py
@@ -6,7 +6,6 @@ from imagekit.models import ProcessedImageField
 from DomePortfolio.lib.images.specs import ImageSpec, ThumbnailSpec
 from DomePortfolio.lib.images.types import BaseImage
 from DomePortfolio.lib.storage.gcp_storage import ImageStorage, FileStorage
-import uuid
 
 
 class Item(models.Model):
@@ -36,34 +35,4 @@ class Image(BaseImage):
     )
 
 
-class Order(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    product = models.ForeignKey(
-        to=Item,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name="orders"
-    )
-    completed = models.BooleanField(default=False)
-    related_paypal_order = models.CharField(max_length=100)
-
-    created_on = models.DateTimeField(auto_now_add=True)
-    ordered_on = models.DateTimeField(null=True)
-
-    customer_email = models.EmailField(null=True)
-    customer_id = models.CharField(max_length=100, null=True)
-
-    amount = MoneyField(
-        max_digits=19, decimal_places=4, default_currency="EUR"
-    )
-    purchased_with_sale = models.PositiveSmallIntegerField(default=0)
-
-    product_downloads_remaining = models.PositiveSmallIntegerField(null=True)
-    download_expires_on = models.DateTimeField(null=True)
-    product_download = models.URLField(null=True)
-
-    def __str__(self) -> str:
-        return f"Order for Item {self.product.pk}; {self.product.title}"
-
-
-__all__ = ["Item", "Image", "Order"]
+__all__ = ["Item", "Image"]
