e2b8f0b247d4593c66ea93df751201e96403ff46
ThaRising <kochbe.ber@gmail.com>
2020-09-10 19:33:55 +0200

    Added Tfvars parser

--- DomePortfolio/lib/utils.py
+++ DomePortfolio/lib/utils.py
@@ -1,4 +1,6 @@
 from uuid import UUID
+from typing import Union
+from pathlib import Path
 
 
 def uuid4_is_valid(uuid: str) -> bool:
@@ -9,4 +11,36 @@ def uuid4_is_valid(uuid: str) -> bool:
     return val.hex == uuid
 
 
-__all__ = ["uuid4_is_valid"]
+class AttrDict(dict):
+    __slots__ = []
+    __doc__ = ""
+
+    def __getattr__(self, item):
+        return super(AttrDict, self).__getitem__(item)
+
+
+class _TfvarsParser:
+    def __init__(self, path: Path) -> None:
+        self.path = path
+
+    def read(self) -> dict:
+        with open(self.path, "r") as fin:
+            c = fin.readlines()
+        extract = lambda i: [el.strip() for el in [line.split("=")[i] for line in c]]
+        keys = extract(0)
+        vals = [line[1:-1].encode() for line in extract(1)]
+        kvpairs = {k: v for k, v in zip(keys, vals)}
+        return kvpairs
+
+
+class Tfvars:
+    def __init__(self, /, path: Union[str, Path]) -> None:
+        if not isinstance(path, Path):
+            path = Path(path)
+        if not path.exists():
+            raise FileNotFoundError(f"The directory {path} does not exist")
+        self.path = path
+        self.vars = AttrDict(**_TfvarsParser(path).read())
+
+
+__all__ = ["uuid4_is_valid", "Tfvars"]
