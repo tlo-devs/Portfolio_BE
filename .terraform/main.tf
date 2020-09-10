be922e319eb2cec618099b61ff97b1e956bac211
ThaRising <kochbe.ber@gmail.com>
2020-09-10 19:33:46 +0200

    Preparations for deployment

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -3,16 +3,26 @@ provider "google" {
   region = "eu-central-3"
 }
 
-resource "google_sql_database" "database" {
-  name = "main-database"
-  instance = google_sql_database_instance.instance
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
 }
 
-resource "google_sql_database_instance" "instance" {
-  name = "main-database-instance"
+resource "google_sql_database_instance" "dbf1" {
+  name = "main-database-instance-${random_id.db_name_suffix.hex}"
   settings {
     tier = "db-f1-micro"
   }
+}
+
+resource "google_sql_database" "database" {
+  name = "domeportfolio-main-database"
+  instance = google_sql_database_instance.dbf1.name
+}
+
+resource "google_sql_user" "users" {
+  name     = var.db_username
+  instance = google_sql_database_instance.dbf1.name
+  password = var.db_password
 }
 
 resource "google_storage_bucket" "images" {
---
+++ .terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "db_password" {
+  description = "Password for the root user of the database"
+}
+
+variable "db_username" {
+  description = "Root user of the database"
+}
