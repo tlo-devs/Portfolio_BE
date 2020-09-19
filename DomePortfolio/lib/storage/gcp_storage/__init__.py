cdd03290c0fae4568adc61f5e57f84e889278a35
ThaRising <kochbe.ber@gmail.com>
2020-09-19 11:30:00 +0200

    Custom naming hook

--- DomePortfolio/lib/storage/gcp_storage/__init__.py
+++ DomePortfolio/lib/storage/gcp_storage/__init__.py
@@ -9,5 +9,8 @@ class ImageStorage(GCPStorage):
 class FileStorage(GCPStorage):
     bucket_name = settings.GCP_BUCKETS.get("files")
 
+    def get_valid_name(self, name: str) -> str:
+        return name
+
 
 __all__ = ["GCPStorage", "ImageStorage", "FileStorage"]
